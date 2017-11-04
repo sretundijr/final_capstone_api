892f3c1b1c2c1d5dbe57f51908a9fab273d29afc
sretundijr <sretundijr@gmail.com>
2017-11-04 16:47:46 -0600

    mock customer model changed and filter function added

--- src/router/advisor-dashboard.js
+++ src/router/advisor-dashboard.js
@@ -11,9 +11,24 @@ const mockCustomerList = require('./mock-customer-list');
 // todo is this right, I havent seen an example that shows this
 router.use(jsonParser);
 
+const filterCustomerResults = () => {
+  const customerDataOnly = mockCustomerList().filter((item) => {
+    if (item.completedQuestionnaire) {
+      return {
+        id: item.id,
+        customerName: item.customerName,
+        customerEmail: item.customerEmail,
+        appointmentDate: item.appointmentDate,
+      };
+    }
+  });
+  console.log(customerDataOnly);
+  return customerDataOnly;
+};
+
 // get returned questionnaires
 router.get('/returned', (req, res) => {
-  res.status(200).json(mockCustomerList());
+  res.status(200).json(filterCustomerResults());
 });
 
 // used to send an email called from send email in client
--- src/router/mock-customer-list.js
+++ src/router/mock-customer-list.js
@@ -6,24 +6,80 @@ const mockCustomerList = () => {
       customerName: 'Steve Retundi',
       customerEmail: 'steve@question.com',
       appointmentDate: '11-11-2017',
+      completedQuestionnaire: true,
+      returnedAnswers: [
+        {
+          archived: false,
+          'Is there a driveability issue accompaning the check engine light?': 'hello',
+          'How long has the fault light been on?': 'long time',
+          'Did something happen prior to the light coming on?': 'everything',
+          'Did something happen prior to the light co?': 'everything',
+        },
+      ],
     },
     {
       id: Math.floor(Math.random() * 10000) + 1,
       customerName: 'Christina',
       customerEmail: 'christina@question.com',
       appointmentDate: '12-14-2017',
+      completedQuestionnaire: true,
+      returnedAnswers: [
+        {
+          archived: false,
+          'Is there a driveability issue accompaning the check engine light?': 'hello',
+          'How long has the fault light been on?': 'long time',
+          'Did something happen prior to the light coming on?': 'everything',
+          'Did something happen prior to the light co?': 'everything',
+        },
+      ],
     },
     {
       id: Math.floor(Math.random() * 10000) + 1,
       customerName: 'Steve Retundi',
       customerEmail: 'steve@question.com',
       appointmentDate: '11-11-2017',
+      completedQuestionnaire: true,
+      returnedAnswers: [
+        {
+          archived: false,
+          'Is there a driveability issue accompaning the check engine light?': 'hello',
+          'How long has the fault light been on?': 'long time',
+          'Did something happen prior to the light coming on?': 'everything',
+          'Did something happen prior to the light co?': 'everything',
+        },
+      ],
     },
     {
       id: Math.floor(Math.random() * 10000) + 1,
       customerName: 'Christina',
       customerEmail: 'christina@question.com',
       appointmentDate: '12-14-2017',
+      completedQuestionnaire: true,
+      returnedAnswers: [
+        {
+          archived: false,
+          'Is there a driveability issue accompaning the check engine light?': 'hello',
+          'How long has the fault light been on?': 'long time',
+          'Did something happen prior to the light coming on?': 'everything',
+          'Did something happen prior to the light co?': 'everything',
+        },
+      ],
+    },
+    {
+      id: Math.floor(Math.random() * 10000) + 1,
+      customerName: 'Christina',
+      customerEmail: 'christina@question.com',
+      appointmentDate: '12-14-2017',
+      completedQuestionnaire: true,
+      returnedAnswers: [
+        {
+          archived: true,
+          'Is there a driveability issue accompaning the check engine light?': 'hello',
+          'How long has the fault light been on?': 'long time',
+          'Did something happen prior to the light coming on?': 'everything',
+          'Did something happen prior to the light co?': 'everything',
+        },
+      ],
     },
   ];
 };
