934152543a1ff1e1db7ee6a0716f76b677a8bf49
sretundijr <sretundijr@gmail.com>
2017-11-24 11:55:10 -0700

    debug safari issue

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+// const jwt = require('express-jwt');
+// const jwtAuthz = require('express-jwt-authz');
+// const jwksRsa = require('jwks-rsa');
 
 const loginRouter = require('./router/login');
 const questionnaireRouter = require('./router/questionniare');
@@ -18,6 +22,26 @@ const app = express();
 app.use(morgan('common'));
 
 app.use(cors({ origin: CLIENT_ORIGIN }));
+
+// const checkJwt = jwt({
+//   // Dynamically provide a signing key
+//   // based on the kid in the header and
+//   // the signing keys provided by the JWKS endpoint.
+//   secret: jwksRsa.expressJwtSecret({
+//     cache: true,
+//     rateLimit: true,
+//     jwksRequestsPerMinute: 5,
+//     jwksUri: `https://advisor-login.auth0.com/.well-known/jwks.json`,
+//   }),
+
+//   // Validate the audience and the issuer.
+//   audience: 'https://advisor-login.auth0.com/api/v2/',
+//   issuer: `https://advisor-login.auth0.com/`,
+//   algorithms: ['RS256'],
+// });
+
+// const checkScopes = jwtAuthz(['read:messages']);
+
 
 app.use('/api/login', loginRouter);
 
